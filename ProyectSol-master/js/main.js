e6359bddc8091ae457ac5edded5c8aa133ad3b55
ttisurus <54994176+ttisurus@users.noreply.github.com>
2019-09-27 14:34:32 -0300

    Add files via upload

---
+++ ProyectSol-master/js/main.js
@@ -0,0 +1,47 @@
+
+//$((documentdo ).ready(main);
+
+
+ 
+(document).ready(function(){
+	alert("hola");
+
+
+});
+
+
+//FUNCION PARA CREAR USUARIO - 
+/*
+boton, formulario
+capturar los valores que se ingresan y guardarlos en la bsae de datos
+
+
+*/
+
+
+//FUNCION PARA INICIAR SESION
+/*
+boton , campo de usuario contra,
+capturar los valores que se ingresan
+traer los valores de la base de datos
+comparar los ingresados con los traidos en la bse de datos
+
+si 
+
+
+
+
+*/
+
+
+
+
+//FUNCION PARA CARGAR LAS FOTOS
+// crear un metodo que se ejecute cuando arranca la pagina
+
+// va a buscar los ultimos 10 produtos de la ultima temporada
+
+// mostrar los 10 productos en la pagina
+
+
+

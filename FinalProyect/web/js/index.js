0be0c4bb0844011dc1ff41ece7b0d65938c8a950
gauna camila <stephany.gonzalez0403@gmail.com>
2019-11-01 19:37:07 -0300

    Agregado de fotos del grupo

--- FinalProyect/web/js/index.js
+++ FinalProyect/web/js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function ()
         {
             console.log(resp);
             
-            for(let valor of resp){$(".integrantes").append("<div class='content-card'><div class='people'><img src='img/Integrantes/"+ valor._id_foto +".jpg'></div><div class='texto-team'><h4>"+ valor._apellido  +" "+ valor._nombre  +" </h4><p>"+ valor._descripcion  +"</p></div></div>");                
+            for(let valor of resp){$(".integrantes").append("<div class='content-card'><div class='people'><img class='foto"+valor._id_foto +"' src='img/Integrantes/"+ valor._id_foto +".jpg'></div><div class='texto-team'><h4>"+ valor._apellido  +" "+ valor._nombre  +" </h4><p>"+ valor._descripcion  +"</p></div></div>");                
            
             }                                                      
         }
